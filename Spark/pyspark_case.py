e03ca2a378ad3062cf27e3f5584393573e68e270
python wei <rav009_student@sina.com>
2018-07-22 12:13:37 +0800

    Update pyspark_case.py

--- Spark/pyspark_case.py
+++ Spark/pyspark_case.py
@@ -7,12 +7,12 @@ if __name__ == "__main__":
     conf = SparkConf().setAppName("case_analytics")
     sc = SparkContext(conf=conf)
     # rdd = sc.textFile('adl://intellimax.azuredatalakestore.net/input.txt')
-    wordsRDD = sc.textFile("adl://intellimax.azuredatalakestore.net/input.txt",10)\
+    sentencesRDD = sc.textFile("adl://intellimax.azuredatalakestore.net/input.txt",10)\
 		     .flatMap(lambda x: [i for i in x.split("|")[2:] if i.strip() is not ''])\
 		     .flatMap(lambda x: [i for i in x.split("\\n") if i.strip() is not ''])\
 		     .persist()
-    stop_sentences = wordsRDD.map(lambda x: (x,1))\
-		     	     .reduceByKey(lambda x, y: x+y)\
-		             .filter(lambda x: x[1] > 100)
+    stop_sentences = sentencesRDD.map(lambda x: (x,1))\
+		     	     	 .reduceByKey(lambda x, y: x+y)\
+		                 .filter(lambda x: x[1] > 100)
 
     stop_sentences = sc.broadcast(sentencesRDD.collect())
