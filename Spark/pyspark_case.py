fdf5f628a0200190870dafed50fdab5fcd568339
python wei <rav009_student@sina.com>
2018-07-22 12:12:39 +0800

    Update pyspark_case.py

--- Spark/pyspark_case.py
+++ Spark/pyspark_case.py
@@ -3,23 +3,16 @@
 import pyspark
 from pyspark import SparkContext, SparkConf
 
-
-def gensentence(line):
-    l = line.split("|")
-    for c in l[2:4]:
-        s = c.split("\\n")
-        return [(i, 1) for i in s if i != '']
-
-
 if __name__ == "__main__":
     conf = SparkConf().setAppName("case_analytics")
     sc = SparkContext(conf=conf)
     # rdd = sc.textFile('adl://intellimax.azuredatalakestore.net/input.txt')
-    stop_sentencesRDD = sc.textFile("adl://intellimax.azuredatalakestore.net/input.txt",10)\
+    wordsRDD = sc.textFile("adl://intellimax.azuredatalakestore.net/input.txt",10)\
 		     .flatMap(lambda x: [i for i in x.split("|")[2:] if i.strip() is not ''])\
 		     .flatMap(lambda x: [i for i in x.split("\\n") if i.strip() is not ''])\
-		     .map(lambda x: (x,1))\
-		     .reduceByKey(lambda x, y: x+y)\
-		     .filter(lambda x: x[1] > 100)
+		     .persist()
+    stop_sentences = wordsRDD.map(lambda x: (x,1))\
+		     	     .reduceByKey(lambda x, y: x+y)\
+		             .filter(lambda x: x[1] > 100)
 
     stop_sentences = sc.broadcast(sentencesRDD.collect())
