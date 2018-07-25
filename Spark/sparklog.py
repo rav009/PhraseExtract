78ae7c8cd9178052018650d84d81565551af39cc
rav009 <rav009_student@sina.com>
2018-07-25 22:54:24 +0800

    improve the log system

--- Spark/sparklog.py
+++ Spark/sparklog.py
@@ -6,9 +6,11 @@ from pyspark import SparkContext, SparkConf
 if __name__ == "__main__":
     conf = SparkConf().setAppName("log-test")
     sc = SparkContext(conf=conf)
+    sc.setLogLevel("WARN")
 
     c = sc.textFile("adl://intellimax.azuredatalakestore.net/test.txt", 10) \
         .count()
     log4jLogger = sc._jvm.org.apache.log4j
     log = log4jLogger.LogManager.getLogger(__name__)
+    
     log.warn("Hello World!")
